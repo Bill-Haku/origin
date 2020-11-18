6d4b3331aba8cbb3bb2f3650f6b846556e41f4f5
HakuBill <battleandwar@hotmail.com>
2020-11-18 20:55:51 +0800

    7

---
+++ ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  week1
+//
+//  Created by HakuBill on 2020/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .fixedSize()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
