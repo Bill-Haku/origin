5d82e0ac09111ca85fc23f95d47e62c1f6f5a908
HakuBill <battleandwar@hotmail.com>
2020-11-19 18:58:20 +0800

    week 1 submit 2

---
+++ Hello World.swift
@@ -0,0 +1,16 @@
+// The new Hello World Program to test git
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some view {
+        Text("Hello, world! --Bill. (a test of git)")
+            .font(.title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider  {
+    static var previews: some view {
+        ContentView()
+    }
+}
