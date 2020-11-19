4ef84c43d6931f90374a2f29d4f7583597324eeb
HakuBill <battleandwar@hotmail.com>
2020-11-19 18:39:14 +0800

    8

---
+++ week1App.swift
@@ -0,0 +1,17 @@
+//
+//  week1App.swift
+//  week1
+//
+//  Created by HakuBill on 2020/11/17.
+//
+
+import SwiftUI
+
+@main
+struct week1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
