b2fa2efc2a354ce4be4911802cd9526db23f37c4
HakuBill <battleandwar@hotmail.com>
2020-11-24 19:30:52 +0800

    20201124

--- Student/Student/main.swift
+++ Student/Student/main.swift
@@ -2,43 +2,37 @@
 //  main.swift
 //  Student
 //
+//  iOS Training Program Task 2
 //  Created by HakuBill on 2020/11/19.
 //
 
 import Foundation
 
 class Human {
-    var gender: Int
-    var age: Int
-    var name: String
-    init(name: String, gender: Int, age: Int) {
-        self.name = name
-        self.age = age
-        self.gender = gender
+    var gender: String = "Female"
+    var age: Int = 19
+    var name: String = "Haku"
+    func printHuman() {
+        print("Human: \(hum.name)\nAge: \(hum.age)\nGender: \(hum.gender)\n")
     }
 }
 
 class student: Human {
-    var college: String
-    var school: String
-    var grade: Int
-    var classnum: Int
-    private var stunum: Int
-    init(college: String, school: String, grade: Int, classnum: Int, stunum: Int) {
-        super.init(name: name, gender: gender, age: age)
-        self.college = college
-        self.school = school
-        self.grade = grade
-        self.classnum = classnum
-        self.stunum = stunum
+    var college: String = "UESTC"
+    var school: String = "SISE"
+    var grade: Int = 1
+    var classnum: Int = 20
+    private var stunum: Int = 1
+    func printStu() {
+        print("Student: \(stu.name)\nAge: \(stu.age)\nGender: \(stu.gender)\nStudent number: \(stu.stunum)\nCollege: \(stu.college)\nSchool: \(stu.school)\nGrade: \(stu.grade)\nClass: \(stu.classnum)\n")
     }
 }
 
-let hum = Human(name: "Bill", gender: 1, age: 18)
-let stu = student(college: "UESTC", school: "SISE", grade: 1, classnum: 20, stunum: 1)
-stu.gender = 0
+let hum = Human()
+let stu = student()
+stu.gender = "Male"
 stu.age = 18
 stu.name = "Haku"
 
-print("Hello, World!")
-
+hum.printHuman()
+stu.printStu()
